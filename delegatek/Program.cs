07921a1893d446d60a9f403a771bb32ed2e319de
attila832558 <ubikutya2003@gmail.com>
2022-12-09 10:58:44 +0100

    v1

---
+++ delegatek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace delegatek
+{
+    class Program
+    {
+        /*delegate*/
+        internal delegate double matekozas(double szam);
+        public static double negyzet(double szam)
+        {
+            return szam * szam;
+        }
+        public static double negyzetgyok(double szam)
+        {
+            return Math.Sqrt(szam);
+        }
+        public static double logaritmus(double szam)
+        {
+            return Math.Log10(szam);
+        }
+       
+        public static double Szamol(double szam, matekozas matek)
+        {
+            return matek(szam);
+        }
+        /*main*/
+        static void Main(string[] args)
+        {
+            matekozas matek = negyzet;
+            WriteLine(Szamol(5, matek));
+            matek = negyzetgyok;
+            WriteLine(Szamol(25, matek));
+            matek = logaritmus;
+            WriteLine(Szamol(100, matek));
+        }
+        
+
+    }
+}
